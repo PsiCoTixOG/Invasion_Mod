f8d0727bb4110f2c98f0c012c3f3a3927c29e070
PsiCoTixOG <steven.curley@gmail.com>
2014-02-10 00:37:57 -0800

    Update Packet Handler
    sorta just added the unimplemented method.. which is technically the
    same, yet a different packet; no idea if this will do it...

--- invmod/common/PacketHandlerCommon.java
+++ invmod/common/PacketHandlerCommon.java
@@ -2,15 +2,23 @@ package invmod.common;
 
 import cpw.mods.fml.common.network.IPacketHandler;
 import cpw.mods.fml.common.network.Player;
-import net.minecraft.nbt.NBTBase;
+import net.minecraft.network.INetworkManager;
 import net.minecraft.network.packet.Packet103SetSlot;
+import net.minecraft.network.packet.Packet250CustomPayload;
 
-public class PacketHandlerCommon
-  implements IPacketHandler
+public class PacketHandlerCommon implements IPacketHandler
 {
-  public void onPacketData(NBTBase manager, Packet103SetSlot packet, Player player)
+  public void onPacketData(INetworkManager manager, Packet103SetSlot packet, Player player)
   {
+  
   }
+
+@Override
+public void onPacketData(INetworkManager manager, Packet250CustomPayload packet, Player player) 
+{
+	// TODO Auto-generated method stub
+	
+}
 }
 
 /* Location:           C:\Users\PsiCoTix\Downloads\_NOOBHAUS\MCDev\DeOp\DeOpInvasionMod.zip
